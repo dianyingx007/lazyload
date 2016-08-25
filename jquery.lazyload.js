3816fcb925ced4647c1633557a65f8192f4725d2
dianyingx007 <dianyingx007@163.com>
2016-08-26 01:56:55 +0800

    bug fix

--- jquery.lazyload.js
+++ jquery.lazyload.js
@@ -1,6 +1,7 @@
 (function($) {
 	$.fn.lazyload = function(option) {
 		var defaults = {
+			attr: "data-url",
 			container: $(window),
 			callback: $.noop
 		};
@@ -40,7 +41,7 @@
 				if(o) {
 					post = o.offset().top - contop, posb = post + o.height();
 					console.log(1);
-					if(o.is(':visiable') && (post >= 0 && post < contHeight) || (posb > 0 && posb <= contHeight)) {
+					if(o.is(':visible') && (post >= 0 && post < contHeight) || (posb > 0 && posb <= contHeight)) {
 						if(!url) {
 							callback(o); /*无地址直接触发回调函数*/
 						} else if(tag === "img") {
