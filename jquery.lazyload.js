374013f74da4e6b01d967022f90dae974e2d301f
dianyingx007 <dianyingx007@163.com>
2016-08-26 00:52:46 +0800

    data regroup

--- jquery.lazyload.js
+++ jquery.lazyload.js
@@ -4,6 +4,18 @@
 			attr: "data-url",
 			container: $(window),
 			callback: $.noop
-		}
+		};
+		var params = $.extend({},defaults,option||{});
+		params.cache = [];
+		$(this).each(function(){
+			var node = this.nodeName.toLowerCase(),url = $(this).attr(params["attr"]);
+			/*重组存放到cache*/
+			var data = {
+				obj: $(this),
+				tag: node,
+				url: url
+			};
+			params.cache.push(data);
+		});
 	}
 })(jQuery);
