edf1f3acc65d2f04bd163406646431f2ae24c6ec
dianyingx007 <dianyingx007@163.com>
2016-08-26 13:51:20 +0800

    v1.2 增加参数threshold

--- jquery.lazyload.js
+++ jquery.lazyload.js
@@ -3,7 +3,8 @@
 		var defaults = {
 			attr: "data-url",
 			container: $(window),
-			callback: $.noop
+			callback: $.noop,
+			threshold: 0
 		};
 		var params = $.extend({}, defaults, option || {});
 		params.cache = [];
@@ -41,7 +42,7 @@
 				if(o) {
 					post = o.offset().top - contop, posb = post + o.height();
 					console.log(1);
-					if(o.is(':visible') && ((post >= 0 && post < contHeight) || (posb > 0 && posb <= contHeight))) {
+					if(o.is(':visible') && ((post >= 0 && post < contHeight+params.threshold) || (posb > 0 && posb <= contHeight))) {
 						if(!url) {
 							callback(o); /*无地址直接触发回调函数*/
 						} else if(tag === "img") {
